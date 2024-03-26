7108d09be30fd234e43cc0a657549a65ea4e5c50
JonathanP341 <159173716+JonathanP341@users.noreply.github.com>
2024-03-26 17:42:23 -0400

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,14 +1,16 @@
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 public class Interface {
     public static void main(String[] args) {
         //Name of the input file will be stored in args[0]
         //Creating a BSTDictionarty object
         BSTDictionary dict = new BSTDictionary();
+        ArrayList<String> labels = new ArrayList<String>();
         //Try catch to read the file
         try {
             //Getting a buffered reader object to go through the given file
-            File file = new File("src/test.txt");
+            File file = new File("small.txt");
             BufferedReader br = new BufferedReader(new FileReader(file));
             String tempLabel; //Storing the input from the file
             String tempTypeData;
@@ -16,6 +18,7 @@ public class Interface {
             Record d;
             while ((tempLabel = br.readLine()) != null && (tempTypeData = br.readLine()) != null) { //Looping until temp = null
                 //Templabel stores the label for the key k and tempTypeData
+                labels.add(tempLabel); //Adding each temp label to the array of labels
                 switch (tempTypeData.charAt(0)) {
                     case '-': //Sound file
                         tempTypeData = tempTypeData.substring(1); //Removing the first part of the string
@@ -56,6 +59,9 @@ public class Interface {
 
         //Getting user input
         StringReader keyboard = new StringReader();
+        SoundPlayer sp = new SoundPlayer();
+        PictureViewer pv = new PictureViewer();
+        ShowHTML show = new ShowHTML();
         String[] words;
         String line = "";
         Record d;
@@ -84,7 +90,11 @@ public class Interface {
                     if (d == null) {
                         System.out.println("There is no sound file for " + words[1]);
                     } else {
-                        System.out.println(d.getDataItem());
+                        try {
+                            sp.play(d.getDataItem());
+                        } catch (MultimediaException e) {
+                            System.out.println("Sound file error for " + words[1]);
+                        }
                     }
                     break;
                 case "play":
@@ -92,15 +102,35 @@ public class Interface {
                     if (d == null) {
                         System.out.println("There is no music file for " + words[1]);
                     } else {
-                        System.out.println(d.getDataItem());
+                        try {
+                            sp.play(d.getDataItem());
+                        } catch (MultimediaException e) {
+                            System.out.println("Sound file error for " + words[1]);
+                        }
                     }
                     break;
                 case "say":
                     d = dict.get(new Key(words[1], 5));
                     if (d == null) {
+                        System.out.println("There is no voice file for " + words[1]);
+                    } else {
+                        try {
+                            sp.play(d.getDataItem());
+                        } catch (MultimediaException e) {
+                            System.out.println("Sound file error for " + words[1]);
+                        }
+                    }
+                    break;
+                case "show":
+                    d = dict.get(new Key(words[1], 6));
+                    if (d == null) {
                         System.out.println("There is no image file for " + words[1]);
                     } else {
-                        System.out.println(d.getDataItem());
+                        try {
+                            pv.show(d.getDataItem());
+                        } catch (MultimediaException e) {
+                            System.out.println("Show file error for " + words[1]);
+                        }
                     }
                     break;
                 case "animate":
@@ -108,7 +138,11 @@ public class Interface {
                     if (d == null) {
                         System.out.println("There is no animated image file for " + words[1]);
                     } else {
-                        System.out.println(d.getDataItem());
+                        try {
+                            pv.show(d.getDataItem());
+                        } catch (MultimediaException e) {
+                            System.out.println("Animated file error for " + words[1]);
+                        }
                     }
                     break;
                 case "browse":
@@ -116,7 +150,7 @@ public class Interface {
                     if (d == null) {
                         System.out.println("There is no webpage " + words[1]);
                     } else {
-                        System.out.println(d.getDataItem());
+                        show.show(d.getDataItem());
                     }
                     break;
                 case "delete":
@@ -126,13 +160,51 @@ public class Interface {
                         System.out.println("No record in the dictionary has key (" + words[1] + ", " + words[2] + ")");
                     }
                     break;
+                case "add":
+                    try {
+                        d = new Record(new Key(words[1], Integer.parseInt(words[2])), words[3]);
+                        dict.put(d);
+                    } catch (DictionaryException e) {
+                        System.out.println("A record with the given key (" + words[1] + ", " + words[2] + ") is already in the ordered dictionary");
+                    }
+                    break;
+                case "list":
+                    //Looping through all of the labels in the arraylist
+                    String output = ""; //String to concatenate output to
+                    for (String label: labels) {
+                        if (label.startsWith(words[1])) {
+                            output += label;
+                            output += ", ";
+                        }
+                    }
+                    if (output.endsWith(", ")) {
+                        System.out.println(output.trim().substring(0, output.length() - 1)); //Trim output then
+                    } else {
+                        System.out.println("No label attributes in the ordered dictionary start with prefix " + args[1]);
+                    }
+                    break;
+                case "first":
+                    d = dict.smallest();
+                    if (d == null) {
+                        System.out.println("No smallest in the dictionary");
+                    } else {
+                        System.out.println(d.getKey().getLabel() + "," + d.getKey().getType() + "," + d.getDataItem());
+                    }
+                    break;
+                case "last":
+                    d = dict.largest();
+                    if (d == null) {
+                        System.out.println("No largest in the dictionary");
+                    } else {
+                        System.out.println(d.getKey().getLabel() + "," + d.getKey().getType() + "," + d.getDataItem());
+                    }
+                    break;
+                case "exit":
+                    System.exit(0); //Might not need this
+                    break;
+                default:
+                    System.out.println("Invalid Command");
             }
-
-
         }
-
-
     }
-
-
 }
